6fcc930b8dd74f5418288089289c4f67179943af
Анатолий <Toli@MacBook-Pro-Toli.local>
2017-08-20 15:04:46 +0300

    Initial Commit

---
+++ Nooot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nooot
+//
+//  Created by Анатолий on 20.08.17.
+//  Copyright © 2017 Анатолий Модестов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
