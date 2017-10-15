f652906c080bd916fd98366edc4fe4bf4612c099
Shockki <tolimodest@gmail.com>
2017-10-15 17:47:53 +0300

    commit

--- Nooot/Custom Segue/SegueFromRight.swift
+++ Nooot/Custom Segue/SegueFromRight.swift
@@ -14,7 +14,7 @@ class SegueFromRight: UIStoryboardSegue {
         let dst = self.destination
         
         src.view.superview?.insertSubview(dst.view, aboveSubview: src.view)
-        dst.view.transform = CGAffineTransform(translationX: src.view.frame.size.width*2, y: 0) //Double the X-Axis
+        dst.view.transform = CGAffineTransform(translationX: src.view.frame.size.width*2, y: 0)
         UIView.animate(withDuration: 0.25, delay: 0.0, options: UIViewAnimationOptions.curveEaseInOut, animations: {
             dst.view.transform = CGAffineTransform(translationX: 0, y: 0)
         }) { (finished) in
--- Nooot/TextViewController.swift
+++ Nooot/TextViewController.swift
@@ -23,8 +23,11 @@ class TextViewController: UIViewController, UITextViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         historyTextView.delegate = self
-        navigationController?.navigationBar.shadowImage = UIImage()
         doneButton.alpha = 0
+        
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(actionSwipe(swipe:)))
+        rightSwipe.direction = .right
+        view.addGestureRecognizer(rightSwipe)
   
         manager.loadJSON(title: titleName)
         semaphore.wait()
@@ -43,8 +46,17 @@ class TextViewController: UIViewController, UITextViewDelegate {
         super.didReceiveMemoryWarning()
     }
     
+    func actionSwipe(swipe: UISwipeGestureRecognizer) {
+        if swipe.direction == .right {
+            performSegue(withIdentifier: "goHome", sender: self)
+        }
+    }
+    
     func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
         doneButton.alpha = 1
+        if bodyText.isEmpty {
+            historyTextView.text? = ""
+        }
         return true
     }
     
@@ -61,6 +73,9 @@ class TextViewController: UIViewController, UITextViewDelegate {
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         doneButton.alpha = 0
+        if bodyText.isEmpty {
+            historyTextView.text? = NSLocalizedString("Your note...", comment: "")
+        }
         self.view.endEditing(true)
     }
 }
